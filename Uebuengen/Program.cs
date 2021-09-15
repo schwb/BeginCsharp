cb210c71750ccc63861e0ec56d2730f31c06f625
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-15 09:59:12 +0200

    Lösung zu #9.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -6,11 +6,11 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte geben Sie einen Text ein");
+            Console.WriteLine("Bitte geben sie einen Text ein, der durch Semikolons in mehrere Abschnitte geteilt ist.");
             string first = Console.ReadLine();
-            char[] MyChar = {' '};
-            string newstring = first.Trim();
-            Console.WriteLine(newstring);
+            string[] second = first.Split(';');
+            Console.WriteLine(second[0]);
+            Console.WriteLine(second[1]);
         }
     }
 }
