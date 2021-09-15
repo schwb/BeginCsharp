924d3ec9f58d55fde710b5eebd0ae1a131c07163
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-15 07:32:24 +0200

    Übungen

---
+++ Uebuengen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uebuengen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
