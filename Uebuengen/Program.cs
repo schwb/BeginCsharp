0e9410cf39e87b9135877218be09f387a94f0ed5
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-15 10:15:26 +0200

    #10

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -6,11 +6,10 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte geben sie einen Text ein, der durch Semikolons in mehrere Abschnitte geteilt ist.");
+            Console.WriteLine("Bitte geben sie einen Text ein");
             string first = Console.ReadLine();
-            string[] second = first.Split(';');
-            Console.WriteLine(second[0]);
-            Console.WriteLine(second[1]);
+            bool third = first.Contains("abc");
+            Console.WriteLine(third);
         }
     }
 }
