62401d7927ff49ad06fd4c3613d31ff52c424d30
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-15 07:53:44 +0200

    Lösung zu Issue #2.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -6,7 +6,11 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bitte geben sie Zahl eins Ihrer Addition ein.");
+            int first = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bitte geben sie Zahl zwei ihrer Addition ein.");
+            int second = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(first + second);
         }
     }
 }
