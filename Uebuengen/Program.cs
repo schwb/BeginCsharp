f7b981ad0b4e2751d033175cd5cb758327702b55
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-16 13:17:14 +0200

    Lösung zu #26.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -6,31 +6,18 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wie ist dein Name?");
-            string a = Console.ReadLine();
-            bool b = string.IsNullOrEmpty(a);
-            while(b)
-            {
-                Console.WriteLine("Hast du keinen Namen?");
-                a = Console.ReadLine();
-                b = string.IsNullOrEmpty(a);
-            }
-            int summe1 = 0;
-            for(int c = 0; c < a.Length; c++)
-            {
-                char u = a[c];
-                if (u.Equals('a'))
-                {
-                    summe1 = summe1 + 1;
-                }
-                if (u.Equals('A'))
-                {
-                    summe1 = summe1 + 1;
-                }
-            }
-            Console.WriteLine("Der Buchstabe A/a ist im eingegebenen Wort " + summe1 + "mal enthalten.");
             Console.WriteLine("Bitte geben sie eine Zahl ein.");
-
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitte geben sie eine Zahl ein.");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if(a>b)
+            {
+                Console.WriteLine("Zahl1 ist größer als Zahl2.");
+            }
+            else
+            {
+                Console.WriteLine("Zahl2 ist größer als Zahl1.");
+            }
         }
     }
 }
