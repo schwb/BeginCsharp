c02147e24ea071e6a1ac8982ef472b7ba31e47ae
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-16 07:23:40 +0200

    Lösung zu #18.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -6,44 +6,17 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wie viele Spalten soll das Array haben?");
-            int tabel = Convert.ToInt32(Console.ReadLine());
-            int[] Einträge = new int[tabel];
-            
-            for (int Index = 0; Index < tabel; Index ++)
-            {
-                Console.WriteLine("Welche Zahl als nächstes?");
-                Einträge[Index] = Convert.ToInt32(Console.ReadLine());
-            }
-            for (int Index = 0; Index < tabel; Index ++)
-            {
-                Console.WriteLine(Einträge[Index]);
-            }
-            int max = Einträge[0];
-            for (int Index = 0; Index < tabel; Index++)
-            {
-                if (max < Einträge[Index])
-                {
-                    max = Einträge[Index];
-                }
-            }
-            Console.WriteLine("Das Maximum lautet " + max);
-            int min = Einträge[0];
-            for (int Index = 0; Index < tabel; Index++)
-            {
-                if (min > Einträge[Index])
-                {
-                    min = Einträge[Index];
-                }
-            }
-            Console.WriteLine("Das Minimum lautet " + min);
-            double summe = 0;
-            for (int d = 0; Index < tabel; d++)
-                summe += Einträge[d];
-                    summe = summe / tabel;
-                Console.WriteLine(summe);     
+            Console.WriteLine("Geben Sie eine zeichenfolge ein.");
+            string a = Console.ReadLine();
+            Console.WriteLine("Ab wo soll der Substring starten?");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bis wohin soll der Substring gehen?");
+            int c = Convert.ToInt32(Console.ReadLine());
+            string d = a.Substring(b, c);
+            Console.WriteLine(d);
         }
     }
 }
+
 
 
