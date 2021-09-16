57018772208af93117b4d227f1bf1d4e21ab7dc1
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-16 07:38:41 +0200

    Aufbau der for Methode, lösung zu #19

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -6,14 +6,13 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Geben Sie eine zeichenfolge ein.");
-            string a = Console.ReadLine();
-            Console.WriteLine("Ab wo soll der Substring starten?");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Bis wohin soll der Substring gehen?");
-            int c = Convert.ToInt32(Console.ReadLine());
-            string d = a.Substring(b, c);
-            Console.WriteLine(d);
+          for (/*Startbedingung*/;/*Abbruchbedingung*/;/*Zählverhalten*/ )
+            {
+                if ( /* Bedingung */)
+                {
+                    //block wird ausgeführt, wenn true
+                }
+            }
         }
     }
 }
