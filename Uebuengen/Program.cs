7ea75c73259a8116a80edd8269a27548738d6e78
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-16 10:37:47 +0200

    Lösung zu #25.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -23,7 +23,30 @@ namespace Uebuengen
                 Console.WriteLine("Welche Zahl ist gesucht?");
                 a = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("Sie haben gewonnen!");
+            Console.WriteLine("Sie haben gewonnen! Möchten Sie nochmal spielen? (yes or no)");
+            string z = Console.ReadLine();
+            string yes = ("yes");
+            while(z == yes)
+            {
+                Console.WriteLine("Welche Zahl ist gesucht?");
+                a = Convert.ToInt32(Console.ReadLine());
+                while (a != zufallszahl)
+                {
+                    if (a < zufallszahl)
+                    {
+                        Console.WriteLine("Deine Zahl ist zu klein");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Deine Zahl ist zu groß");
+                    }
+                    Console.WriteLine("Welche Zahl ist gesucht?");
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("Sie haben gewonnen! Möchten Sie nochmal spielen? (yes or no)");
+                string d = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye.");
         }
     }
 }
