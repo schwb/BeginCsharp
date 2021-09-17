39436360e347d6819bf34fd770c461a194314fbd
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-17 09:04:38 +0200

    Lösung zu #29.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -6,34 +6,35 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            double summe1 = 0;
-            Console.WriteLine("Wie viele Einträge möchtest du im Array haben?");
-            int Einträge = Convert.ToInt32(Console.ReadLine());
-            double[] Array = new double[Einträge];
-            for (int Index = 0; Index < Array.Length; Index++)
+            Console.WriteLine("Bitte gib eine Zahl ein.");
+            double Zahl1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte gib eine Zahl ein.");
+            double Zahl2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte gib einen Operator ein. (+-*/%)");
+            string Operator = Console.ReadLine();
+            double ergebnis = 0;
+            switch (Operator)
             {
-                Console.WriteLine("Bitte geben sie eine Fließkommazahl ein.");
-                Array[Index] = Convert.ToDouble(Console.ReadLine());
+                case "+":
+                    ergebnis = Zahl1 + Zahl2;
+                    break;
+                case "-":
+                    ergebnis = Zahl1 - Zahl2;
+                    break;
+                case "/":
+                    ergebnis = Zahl1 / Zahl2;
+                    break;
+                case "*":
+                    ergebnis =  Zahl1* Zahl2;
+                    break;
+                case "%":
+                    ergebnis = Zahl1 % Zahl2;
+                    break;
+                default:
+                    Console.WriteLine("Der von Dir eingegebene Operator ist nicht definiert");
+                    break;
             }
-            for (int Index = 0; Index < Array.Length; Index++)
-            {
-                summe1 += Array[Index];
-            }
-            Console.WriteLine($"Summe for-Schleife = {summe1}");
-            double summe2 = 0;
-            foreach(double value in Array )
-            {
-                summe2 += value;
-            }
-            Console.WriteLine($"Summe foreach-Schleife = {summe2}");
-            double summe3 = 0;
-            int a = 0;
-            while(a != Array.Length)
-                    {
-                summe3 += Array[a];
-                a += 1;
-            }
-            Console.WriteLine($"Summe while-Schleife = {summe3}");
+            Console.WriteLine($"Das Ergebnis deiner Rechnung ist {ergebnis}.");
         }
     }
 }
