5e30193348be1ffeb34c29a32d4d1f84ac1fac92
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-17 17:57:17 +0200

    Lösung zu #35

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -7,9 +7,11 @@ namespace Uebuengen
         static void Main(string[] args)
         {
             Console.WriteLine("Bitte gib eine Zahl ein.");
-            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+            string Zahl3 = Console.ReadLine();
+            int Zahl1 = Convert.ToInt32(Zahl3.Replace("-",""));
             Console.WriteLine("Bitte gib eine Zahl ein.");
-            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+            string Zahl4 = Console.ReadLine();
+            int Zahl2 = Convert.ToInt32(Zahl4.Replace("-",""));
             int G = 0;
             int K = 0;
             if (Zahl1>=Zahl2)
@@ -25,15 +27,17 @@ namespace Uebuengen
             int u = G + 1;
             int[] Z = new int[u];
             int i = 0;
-            for (int Index=u; i==0 && Index>=0 ;Index--)
+            for (int Index=u; i==0;Index--)
             {
-                if (Zahl1%Index==0 && Zahl2%Index==0)
+                if (Zahl1==0 && Zahl2==0)
                 {
-                    Console.WriteLine($"Der größte gemeinsame Teiler ist{Index}.");
+                    Console.WriteLine("Der größte gemeinsame Teiler ist Unendlich.");
                     i = 1;
                 }
-                else
+                else if (Zahl1%Index==0 && Zahl2%Index==0)
                 {
+                    Console.WriteLine($"Der größte gemeinsame Teiler ist {Index}.");
+                    i = 1;
                 }
             }
         }
