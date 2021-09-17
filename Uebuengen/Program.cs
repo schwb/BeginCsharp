91ea7f5daa53e1561d3c13164eee136e76b37a63
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-17 15:10:14 +0200

    vorige lösung war falsch. Das ist die lösung zu #33.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -13,12 +13,13 @@ namespace Uebuengen
             int c = 0;
             for (int Index=0; Index<nummer.Length-1; Index++)
             {
-                int z = Convert.ToInt32(nummer[Index]);
+                int z = Convert.ToInt32(nummer[Index].ToString());
                 int b = z * a;
                 a -= 1;
                 c += b;
             }
-            int x = Convert.ToInt32(nummer[9]);
+            Console.WriteLine(c);
+            int x = Convert.ToInt32(nummer[9].ToString());
             if ((c + x) % 11 == 0)
             {
                 Console.WriteLine($"Diese Nummer ist {(c + x) / 11} *11, also ist sie eine gültige ISBN-Nummer.");
