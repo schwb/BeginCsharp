14ed56f36a055530794dc4c8677eb905f6c590f6
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-21 07:20:51 +0200

    Merke diesen Aufbau.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -7,33 +7,23 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            List<int> Zahlen = new List<int>() { 5, 8, 3, 4, 5 };
-            Sum(Zahlen);
-            Sum(5, 8);
-            string a = "bibi";
-                string b = "bloxberg";
-            Console.WriteLine(Kontatiniert(a,b));
+            double x = 3;
+            double y = 4;
+            (x,y) = FalseSwap(x, y);
+
+            Console.WriteLine($"x: {x}\t y: {y}");
         }
-        static void Sum(List<int> Eingesetzt)
+
+        static (double, string ) FalseSwap(double a, double b)
         {
-                int summe = 0;
-                for (int Index =0; Index < Eingesetzt.Count; Index++)
-                {
-                    summe += Eingesetzt[Index];
-                }
-                Console.WriteLine(summe);
+            
+            double temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"a: {a} und b:{b}");
+            return (a, b.ToString());
+
         }
-            static void Sum(int Eingesetzt,int Eingesetzt1)
-            {
-                int summe = Eingesetzt + Eingesetzt1;
-                Console.WriteLine(summe);
-            }
-            //void gibt nichts zurück. Hier werden durch console.writeline inhslte ausgegeben.
-            static string Kontatiniert(string eingesetzt, string eingesetzt1)
-            {
-                return(string.Join(';', eingesetzt, eingesetzt1));
-            }
-            //um Inhalte zurückzugeben sird statt void der Datentyp geschrieben und die Ausgabe in der Methode durch return festgelegt.        
     }
 }
 
