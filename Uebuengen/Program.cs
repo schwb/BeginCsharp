109236201db7a44a70edb04da94887053c3bea41
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-21 09:00:23 +0200

    Lösung zu #43.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -7,18 +7,53 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            int a = 3;
-            string b = "3.14";
-            double c = 15.6;
-            (b, c, a) = DreichfachSwap(a, b, c);
-            Console.WriteLine($"{b}, {c}, {a}");
+            Console.WriteLine("Bitte gib eine Zahl ein.");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitte gib eine Zahl ein.");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitte gib eine Rechenoperation ein. (+,-,*,/)");
+            string Operant = Console.ReadLine();
+            switch (Operant)
+            {
+                case "+":
+                    int Ergebnis = Addition(Zahl1, Zahl2);
+                    Console.WriteLine($"Das Ergebnis deiner Addition lautet: {Ergebnis}.");
+                    break;
+                case "-":
+                    Ergebnis = Subtraktion(Zahl1, Zahl2);
+                    Console.WriteLine($"Das Ergebnis deiner Subtraktion lautet: {Ergebnis}.");
+                    break;
+                case "*":
+                    Ergebnis = Multiplikation(Zahl1, Zahl2);
+                    Console.WriteLine($"Das Ergebnis deiner Multiplikation lautet: {Ergebnis}");
+                    break;
+                case "/":
+                    Ergebnis = Division(Zahl1, Zahl2);
+                    Console.WriteLine($"Das Ergebnis deiner Division lautet: {Ergebnis}.");
+                    break;
+                default:
+                    Console.WriteLine("Der von dir eingegebene Operator ist nicht definiert");
+                    break;
+            }
         }
-        static (string, double, int) DreichfachSwap(int egal, string was, double hiersteht)
+        static int Addition (int egal, int auchegal)
         {
-            string d = Convert.ToString(egal);
-            double e = Convert.ToDouble(was);
-            int f = Convert.ToInt32(hiersteht);
-            return (d, e, f);
+            int a = egal + auchegal;
+            return (a);
+        }
+        static int Subtraktion (int egal, int auchegal)
+        {
+            int a = egal - auchegal;
+            return (a);
+        }
+        static int Multiplikation(int egal, int auchegal)
+        {
+            int a = egal * auchegal;
+            return (a);
+        }
+        static int Division(int egal, int auchegal)
+        {
+            return (egal / auchegal);
         }
     }
 }
