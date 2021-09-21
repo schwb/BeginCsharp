7fa5272f355ce52a6adfa7e8a8d3ac6fdbca699d
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-21 09:53:07 +0200

    Zwischenschritt

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -7,53 +7,50 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte gib eine Zahl ein.");
-            int Zahl1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Bitte gib eine Zahl ein.");
-            int Zahl2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Bitte gib eine Rechenoperation ein. (+,-,*,/)");
-            string Operant = Console.ReadLine();
-            switch (Operant)
+            Console.WriteLine("Bitte gib den Nenner des Bruchs ein.");
+            double Zahl2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte gib den Zähler des Bruchs ein.");
+            double Zahl1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte wähle eine Rechenmethode für den Bruch.");
+            string Rechenmethode = Console.ReadLine();
+            double Ergebnis = 0;
+            switch(Rechenmethode)
             {
-                case "+":
-                    int Ergebnis = Addition(Zahl1, Zahl2);
-                    Console.WriteLine($"Das Ergebnis deiner Addition lautet: {Ergebnis}.");
+                case "Negieren":
+                    Ergebnis = Negieren(Zahl2, Zahl1);
                     break;
-                case "-":
-                    Ergebnis = Subtraktion(Zahl1, Zahl2);
-                    Console.WriteLine($"Das Ergebnis deiner Subtraktion lautet: {Ergebnis}.");
+                case "Umkehren":
+                    Ergebnis = Umkehren(Zahl2, Zahl1);
                     break;
-                case "*":
-                    Ergebnis = Multiplikation(Zahl1, Zahl2);
-                    Console.WriteLine($"Das Ergebnis deiner Multiplikation lautet: {Ergebnis}");
-                    break;
-                case "/":
-                    Ergebnis = Division(Zahl1, Zahl2);
-                    Console.WriteLine($"Das Ergebnis deiner Division lautet: {Ergebnis}.");
+                case "Erweitern":
+                    Console.WriteLine("Um welchen Faktor möchtest du den Bruch erweitern?");
+                    double Faktor = Convert.ToDouble(Console.ReadLine());
+                    Ergebnis = Erweitern(Zahl2, Zahl1, Faktor);
                     break;
                 default:
-                    Console.WriteLine("Der von dir eingegebene Operator ist nicht definiert");
+                    Console.WriteLine("Deine gewählte Methode ist in diesem Programm nicht definiert.");
                     break;
             }
+            Console.WriteLine($"Das Ergebnis lautet {Ergebnis}.");
         }
-        static int Addition (int egal, int auchegal)
+        static double Negieren(double Zahl2, double Zahl1)
         {
-            int a = egal + auchegal;
-            return (a);
+            Zahl1 = Zahl1 * -1;
+            return (Zahl1 / Zahl2);
         }
-        static int Subtraktion (int egal, int auchegal)
+        static double Umkehren(double Zahl2, double Zahl1)
         {
-            int a = egal - auchegal;
-            return (a);
+            return (Zahl2 / Zahl1);
         }
-        static int Multiplikation(int egal, int auchegal)
+        static double Erweitern(double zahl2, double zahl1, double faktor)
         {
-            int a = egal * auchegal;
-            return (a);
+            zahl1 = zahl1 * faktor;
+            zahl2 = zahl2 * faktor;
+            return (zahl1 / zahl2);
         }
-        static int Division(int egal, int auchegal)
+        static double Ggt(double zahl2, double zahl1)
         {
-            return (egal / auchegal);
+
         }
     }
 }
