3fc8d9c032075044c574b0403282401a31688da6
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-21 08:11:04 +0200

    Lösung zu #42.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -7,33 +7,19 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            List<int> Zahlen = new List<int>() { 5, 8, 3, 4, 5 };
-            Sum(Zahlen);
-            Sum(5, 8);
-            string a = "bibi";
-                string b = "bloxberg";
-            Console.WriteLine(Kontatiniert(a,b));
+            int a = 3;
+            string b = "3.14";
+            double c = 15.6;
+            (b, c, a) = DreichfachSwap(a, b, c);
+            Console.WriteLine($"{b}, {c}, {a}");
         }
-        static void Sum(List<int> Eingesetzt)
+        static (string, double, int) DreichfachSwap(int egal, string was, double hiersteht)
         {
-                int summe = 0;
-                for (int Index =0; Index < Eingesetzt.Count; Index++)
-                {
-                    summe += Eingesetzt[Index];
-                }
-                Console.WriteLine(summe);
+            string d = Convert.ToString(egal);
+            double e = Convert.ToDouble(was);
+            int f = Convert.ToInt32(hiersteht);
+            return (d, e, f);
         }
-            static void Sum(int Eingesetzt,int Eingesetzt1)
-            {
-                int summe = Eingesetzt + Eingesetzt1;
-                Console.WriteLine(summe);
-            }
-            //void gibt nichts zurück. Hier werden durch console.writeline inhslte ausgegeben.
-            static string Kontatiniert(string eingesetzt, string eingesetzt1)
-            {
-                return(string.Join(';', eingesetzt, eingesetzt1));
-            }
-            //um Inhalte zurückzugeben sird statt void der Datentyp geschrieben und die Ausgabe in der Methode durch return festgelegt.        
     }
 }
 
