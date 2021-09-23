d1bcaa7d40dfc24935aafcd98907a24f8dfb3d8e
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-23 08:15:25 +0200

    Klasse Dreieck #50.

--- Uebuengen/Dreieck.cs
+++ Uebuengen/Dreieck.cs
@@ -13,17 +13,17 @@ namespace Uebuengen
         public double Seite3 { get; set; }
         public double Basis { get; set; }
         public double Höhe { get; set; }
-        public double Umfang(double Seite1, double Seite2, double Seite3)
+        public double Umfang()
         {
             return (Seite1 + Seite2 + Seite3);
         }
-        public double Fläche(double Basis,double Höhe)
+        public double Fläche()
         {
             return (Basis*Höhe/2);
         }
         public override string ToString()
         {
-            return $"Das Dreieck hat den Umfang {Umfang(Seite1, Seite2, Seite3)} und den Flächeninhalt {Fläche(Basis, Höhe)}";
+            return $"Das Dreieck hat den Umfang {Umfang()} und den Flächeninhalt {Fläche()}";
         }
     }
 }
