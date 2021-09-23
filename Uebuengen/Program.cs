aa26a34e89ef382f243ba50ca96f032b343a3434
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-23 15:15:41 +0200

    Mit Trennstrich #53.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -8,34 +8,44 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            int summe = 0;
             int Position = 0;
             Random r = new Random();
-            int[,] Zahlen = new int[5, 7];
+            int[,] Zahlen = new int[5,7];
             for(int i=0;i<Zahlen.GetLength(0);i++)
             {
-                for(int j=0;j<Zahlen.GetLength(1);j++)
+                if (i == Zahlen.GetLength(0) - 2)
                 {
-                   if(i==Zahlen.GetLength(0)-1)
+                    for (int w = 0; w < (Zahlen.GetLength(1)-1)*6; w++)
                     {
-                        summe = 0;
-                        for(int k=0;k<Zahlen.GetLength(0)-1;k++)
-                        {
-                            summe += Zahlen[k, Position];
-                        }
-                        Zahlen[Zahlen.GetLength(0) - 1, Position]=summe;
-                        Position += 1;
-                        string sum = Convert.ToString(summe);
-                        Console.Write($"{sum.PadRight(5,' ')}");
+                        Console.Write("-");
                     }
-                    else
-                    {
-                        Zahlen[i,j]=r.Next(1, 99);
-                        string Zahl= Convert.ToString(Zahlen[i, j]);
-                        Console.Write($"{Zahl.PadRight(5, ' ')}");
-                    }
+                    Console.Write("\n");
                 }
-                Console.Write("\n");
+                else
+                {
+                    for (int j = 0; j < Zahlen.GetLength(1); j++)
+                    {
+                        if (i == Zahlen.GetLength(0) - 1)
+                        {
+                            int summe = 0;
+                            for (int k = 0; k < Zahlen.GetLength(0) - 1; k++)
+                            {
+                                summe += Zahlen[k, Position];
+                            }
+                            Zahlen[Zahlen.GetLength(0) - 1, Position] = summe;
+                            Position += 1;
+                            string sum = Convert.ToString(summe);
+                            Console.Write($"{sum.PadRight(5, ' ')}");
+                        }
+                        else
+                        {
+                            Zahlen[i, j] = r.Next(1, 99);
+                            string Zahl = Convert.ToString(Zahlen[i, j]);
+                            Console.Write($"{Zahl.PadRight(5, ' ')}");
+                        }
+                    }
+                    Console.Write("\n");
+                }
             }
         }
     }
