9bde7633c9323f44d8bddcfc085ec6b0c9803eac
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-23 08:14:30 +0200

    Klasse Rechteck #50.

--- Uebuengen/Rechteck.cs
+++ Uebuengen/Rechteck.cs
@@ -10,17 +10,17 @@ namespace Uebuengen
     {
         public double Seitenlänge1 { get; set; }
         public double Seitenlänge2 { get; set; }
-        public double Umfang(double Seitenlänge1, double Seitenlänge2)
+        public double Umfang()
         {
             return (2 * (Seitenlänge1 + Seitenlänge2));
         }
-        public double Fläche(double Seitenlänge1, double Seitenlänge2)
+        public double Fläche()
         {
             return (Seitenlänge1* Seitenlänge2);
         }
         public override string ToString()
         {
-            return $"Das Rechteck hat den Umfang {Umfang(Seitenlänge1,Seitenlänge2)} und den Flächeninhalt {Fläche(Seitenlänge1,Seitenlänge2)}.";
+            return $"Das Rechteck hat den Umfang {Umfang()} und den Flächeninhalt {Fläche()}.";
         }
 
     }
