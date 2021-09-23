7f06fb416409d56a0656ccd189fe26725989a2e9
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-23 08:02:38 +0200

    Ausführung der Klassen in der Program.cs Klasse mit Main-Methode #49.

--- Uebuengen/Program.cs
+++ Uebuengen/Program.cs
@@ -8,11 +8,20 @@ namespace Uebuengen
     {
         static void Main(string[] args)
         {
-            Auto Ford = new Auto("Ford");
-            Ford.Räder = 4;
-            Ford.Türen = 3;
-            Ford.Farbe = "schwarz";
-            Console.WriteLine($"Das Auto namens {Ford.Modell} hat {Ford.Räder} Räder, {Ford.Türen} Türen und ist {Ford.Farbe} lackiert.");
+            Dreieck Eins = new Dreieck();
+            Eins.Seite1 = 7.07;
+            Eins.Seite2 = 7.07;
+            Eins.Seite3 = 10;
+            Eins.Basis = 10;
+            Eins.Höhe = 5;
+            Console.WriteLine(Eins.ToString());
+            Quadrat Zwei = new Quadrat();
+            Zwei.Seitenlänge = 10;
+            Console.WriteLine(Zwei.ToString());
+            Rechteck Drei = new Rechteck();
+            Drei.Seitenlänge1 = 15;
+            Drei.Seitenlänge2 = 25;
+            Console.WriteLine(Drei.ToString());
         }
     }
 }
