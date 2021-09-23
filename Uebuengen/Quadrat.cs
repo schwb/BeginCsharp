86e5cb416909dd223cd3ecb1cfe214cf21b4c698
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-23 08:01:59 +0200

    Klasse Quadrat #49.

---
+++ Uebuengen/Quadrat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebuengen
+{
+    class Quadrat
+    {
+        public double Seitenlänge { get; set; }
+        public double Umfang(double Seitenlänge)
+        {
+            return (4 * Seitenlänge);
+        }
+        public double Fläche(double Seitenlänge)
+        {
+            return (Seitenlänge * Seitenlänge);
+        }
+        public override string ToString()
+        {
+            return $"Das Quadrat hat den Umfang {Umfang(Seitenlänge)} und den Flächeninhalt {Fläche(Seitenlänge)}.";
+        }
+    }
+}
