0473388a7994c7ed6ad263f1d399eef950e994f9
TEC\BSC <bernhard.schwab@tecalliance.net>
2021-09-23 08:01:36 +0200

    Klasse Rechteck #49.

---
+++ Uebuengen/Rechteck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebuengen
+{
+    class Rechteck
+    {
+        public double Seitenlänge1 { get; set; }
+        public double Seitenlänge2 { get; set; }
+        public double Umfang(double Seitenlänge1, double Seitenlänge2)
+        {
+            return (2 * (Seitenlänge1 + Seitenlänge2));
+        }
+        public double Fläche(double Seitenlänge1, double Seitenlänge2)
+        {
+            return (Seitenlänge1* Seitenlänge2);
+        }
+        public override string ToString()
+        {
+            return $"Das Rechteck hat den Umfang {Umfang(Seitenlänge1,Seitenlänge2)} und den Flächeninhalt {Fläche(Seitenlänge1,Seitenlänge2)}.";
+        }
+
+    }
+}
